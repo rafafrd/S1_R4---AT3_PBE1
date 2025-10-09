b525f71ff35626566bae71d80c2da9103c71dc19
rafafrd <bolekao03@gmail.com>
2025-10-09 10:35:38 -0300

    atv1

--- server.js
+++ server.js
@@ -2,38 +2,34 @@ const express = require("express");
 const app = express();
 const PORT = 8081;
 
-// 1. Crie um projeto que contenha uma rota POST /alunos que receba:
-// {
-//   "nome": "Ana",
-//   "notas": [8, 7, 9, 6]
-// }
-// Calcular a média e retornar um JSON com nome, média e situação (APROVADO ou REPROVADO).
-// Considere acima de 6 para aprovado.
-
 // middleware para utilização de JSON
 app.use(express.json());
 // ==============================================================================
 app.post("/alunos", (req, res) => {
   try {
     const { nome, notas } = req.body;
+    // Validacao
     if (!nome || !notas) {
-      // Validacao de dados
-      res.status(400).json({ message: "Dados invalidos" });
-    } else {
-      const sum = 0;
-      for (let i = 0; i < notas.length; i++) {
-        // Roda dentro do set do json pegando os valores
+      return res.status(400).json({ message: "O nome e as notas são obrigatórios" });
+    }
+    if (notas.length === 0) {
+      return res.status(400).json({ message: "O campo 'notas' deve ter pelo menos uma nota." });
+    }
+    let sum = 0;
+    for (let i = 0; i < notas.length; i++) {
+      if (typeof notas[i] === 'number') { // verifica se é número
         sum += notas[i];
-      }
-      const media = sum / notas.length; // n de notas
-      if (media >= 6) {
-        res.status(201).json({ nome: nome, media: media, situacao: "APROVADO" });
       } else {
-        res.status(201).json({ nome: nome, media: media, situacao: "REPROVADO" });
+        return res.status(400).json({ message: `A nota '${notas[i]}' na posição ${i} não é um número válido.` });
       }
     }
+    // retorna a média e a situação
+    const media = sum / notas.length;
+    const situacao = media >= 6 ? "APROVADO" : "REPROVADO";
+    return res.status(200).json({ nome: nome, media: media.toFixed(2), situacao: situacao });
+
   } catch (error) {
-    res.status(500).json({ message: "erro", errorMessage: error.message });
+    res.status(500).json({ message: "Erro interno no servidor", errorMessage: error.message });
   }
 });
 // ==============================================================================
