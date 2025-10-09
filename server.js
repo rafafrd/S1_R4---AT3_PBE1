22e78e19112b131853b32fc01de26b3e7a2f79b9
rafafrd <bolekao03@gmail.com>
2025-10-09 11:03:37 -0300

    atv3

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8081;
 
-// Crie um projeto que contenha uma rota POST /soma e receba uma quantidade indefinida de números através do body e realize a soma dos valores:
-// Verifique se os valores são numéricos antes de realizar o cálculo, caso não seja informa ao usuário e não realizar o cálculo.
-// Utilize a função reduce, pesquise em fontes na internet a forma de utilizar.
-
+/** 
+  Crie um projeto que contenha uma rota POST /soma e receba uma quantidade indefinida de números através do body e realize a soma dos valores: 
+  Verifique se os valores são numéricos antes de realizar o cálculo, ignore os não númericos, some os demais..
+  Utilize a função reduce, pesquise em fontes na internet a forma de utilizar.
+*/
 // middleware para utilização de JSON
 app.use(express.json());
 // ==============================================================================
@@ -14,16 +15,24 @@ app.post("/soma", (req, res) => {
     const { num } = req.body;
     // Validacao
     if (!num) {
-      return res.status(400).json({ message: "Insira Valores Validos" });
+      return res.status(400).json({ message: "Insira Valores" });
     } else {
+      let arrayCerta = [];
+      let soma = 0;
+      // lendo array
       for (let i = 0; i < num.length; i++) {
         if (isNaN(num[i])) {
-          return res.status(400).json({ message: "Insira Apenas Valores Numéricos" });
+          console.log(`${num[i]} não é um num`);
+          continue; // pula pro próximo
         } else {
-          const soma = num.reduce((acumulador, valorAtual) => acumulador + valorAtual,0);
-          return res.status(201).json({ soma: soma });
+          arrayCerta[i] = Number(num[i]);
+          console.log("arrayCerta", arrayCerta);
+          // Utilizando reduce
+          soma = arrayCerta.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
+          console.log("soma", soma);
         }
       }
+      return res.status(201).json({ soma: soma });
     }
   } catch (error) {
     res.status(500).json({errorMessage: error.message});
